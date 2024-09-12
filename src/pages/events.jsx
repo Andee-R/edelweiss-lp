1ef4c04bdc1cf60d76608a01ffab04832279c551
packetchaos <cyberdice113@gmail.com>
2024-09-12 11:35:36 -0700

    minor changes and a 404 redirect

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import "../pages/Events.css";
-import taps from "../assets/bier_taps.jpg";
+import biergarten from "../assets/edelweiss_biergarten.jpg";
+import backroom from "../assets/back_room.png";
+import movie from "../assets/Movie_night.png";
+
 function Events() {
   return (
     <div className="events">
-      <div className="eventsText container pb-5">
-        <h1>Upcoming Events</h1>
-        <p
-          className="title pt-3 d-flex justify-content-between
-        ">
-          Oktoberfest - 9/21-10/30
-          <img src={taps} class="img-thumbnail" alt="..." />
-        </p>
-        <p className="title d-flex justify-content-between">
-          <img src={taps} class="img-thumbnail" alt="..." />
-          Movie Night Monday's
-        </p>
-        <p className="title d-flex justify-content-between">
-          Trivia Night Tuesday's
-          <img src={taps} class="img-thumbnail" alt="..." />
-        </p>
+    <div className="eventsText container">
+      <h1>Upcoming Events</h1>
+      <div className="row">
+        <div className="col">
+          <p className="title">Oktoberfest - 9/21-10/30</p>
+          <img src={biergarten} alt="" className="img-thumbnail" />
+        </div>
+        <div className="col">
+          <p className="title">Movie Night Monday's</p>
+          <img src={movie} alt="" className="img-thumbnail" />
+        </div>
+        <div className="col">
+          <p className="title">Trivia Night Tuesday's</p>
+          <img src={backroom} alt="" className="img-thumbnail" />
+        </div>
       </div>
     </div>
+  </div>
   );
 }
 
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -3,21 +3,21 @@ import "../pages/Gallery.css";
 import { GoArrowLeft } from "react-icons/go";
 import { GoArrowRight } from "react-icons/go";
 import taps from "../assets/bier_taps.jpg";
+import pretzel from "../assets/food-pretzel.png"
 
 const Gallery = () => {
   return (
     <div className="gallery">
       <div className="galleryText container">
         <h1> Gallery</h1>
-
-        <div className="images">
-          <div className="slider">
-            <div className="slide">
-              <img src={taps} class="img-thumbnail" alt="..." />
-              <img src={taps} class="img-thumbnail" alt="..." />
-              <img src={taps} class="img-thumbnail" alt="..." />
-              <img src={taps} class="img-thumbnail" alt="..." />
-            </div>
+      </div>
+      <div className="images container">
+        <div className="slider">
+          <div className="slide">
+            <img src={pretzel} class="img-thumbnail" alt="..." />
+            <img src={taps} class="img-thumbnail" alt="..." />
+            <img src={taps} class="img-thumbnail" alt="..." />
+            <img src={taps} class="img-thumbnail" alt="..." />
           </div>
         </div>
       </div>

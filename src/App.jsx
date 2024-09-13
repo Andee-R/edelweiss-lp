71bce82c4ae45d15d7fb88dfec1dddd29ecfa9da
Andee-R <aqueen.reid@gmail.com>
2024-09-13 15:30:47 -0700

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/About/About";
-import Resident from "./pages/Resident";
+import Resident from "./Components/Resident";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Events from "./pages/Events";
@@ -15,8 +15,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/resident" element={<Resident />} />
       </Routes>
+      <Resident />
       <About />
     </>
   );
